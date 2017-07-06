cc34cb61f003338843ff29b1f0c3a7176bdee44e
imamdigmi <imam.digmi@gmail.com>
2017-07-06 22:57:42 +0700

    Update: Alternatif

--- data-alternatif-baru.php
+++ data-alternatif-baru.php
@@ -1,73 +1,61 @@
 <?php
-include_once 'header.php';
-if($_POST){
-	
-	include_once 'includes/alternatif.inc.php';
-	$eks = new Alternatif($db);
+include_once('includes/header.inc.php');
 
+if($_POST){
+	include_once('includes/alternatif.inc.php');
+	$eks = new Alternatif($db);
 	$eks->id = $_POST['kd'];
 	$eks->nm = $_POST['nm'];
-	
-	if($eks->insert()){
-?>
-<script type="text/javascript">
-window.onload=function(){
-	showStickySuccessToast();
-};
-</script>
-<?php
-	}
-	
-	else{
-?>
-<script type="text/javascript">
-window.onload=function(){
-	showStickyErrorToast();
-};
-</script>
-<?php
+	if($eks->insert()){ ?>
+		<script type="text/javascript">
+			window.onload=function(){
+				showStickySuccessToast();
+			};
+		</script> <?php
+	} else { ?>
+		<script type="text/javascript">
+			window.onload=function(){
+				showStickyErrorToast();
+			};
+		</script> <?php
 	}
 }
 ?>
-		<div class="row">
-<!--		  <div class="col-xs-12 col-sm-12 col-md-2">
-		  <?php
-//			include_once 'sidebar.php';
-			?>
-		  </div>-->
-		  <div class="col-xs-12 col-sm-12 col-md-12">
-		  <ol class="breadcrumb">
-			  <li><a href="index.php"><span class="fa fa-home"></span> Beranda</a></li>
-			  <li><a href="data-alternatif.php"><span class="fa fa-book"></span> Data Alternatif</a></li>
-			  <li class="active"><span class="fa fa-clone"></span> Tambah Data</li>
-			</ol>
-		  	<p style="margin-bottom:10px;">
-		  		<strong style="font-size:18pt;"><span class="fa fa-clone"></span> Tambah Alternatif</strong>
-		  	</p>
-		  	<div class="panel panel-default">
-		<div class="panel-body">
-			
-			    <form method="post">
-				  <div class="form-group">
-				    <label for="kd">ID Alternatif</label>
-				    <select class="form-control" id="kd" name="kd">
-						<option>A1</option>
-						<option>A2</option>
-						<option>A3</option>
-						<option>A4</option>
-						<option>A5</option>
-					</select>
-				  </div>
-				  <div class="form-group">
-				    <label for="kt">Nama Alternatif</label>
-				    <input type="text" class="form-control" id="nm" name="nm" required>
-				  </div>
-				  <button type="submit" class="btn btn-primary"><span class="fa fa-save"></span> Simpan</button>
-				  <button type="button" onclick="location.href='data-alternatif.php'" class="btn btn-success"><span class="fa fa-history"></span> Kembali</button>
-				</form>
-			  
-		  </div></div></div>
+<div class="row">
+  <div class="col-xs-12 col-sm-12 col-md-12">
+	  <ol class="breadcrumb">
+		  <li><a href="index.php">Beranda</a></li>
+		  <li><a href="data-alternatif.php">Data Alternatif</a></li>
+		  <li class="active">Tambah Data</li>
+		</ol>
+  	<p style="margin-bottom:10px;">
+  		<strong style="font-size:18pt;"><span class="fa fa-clone"></span> Tambah Alternatif</strong>
+  	</p>
+  	<div class="panel panel-default">
+			<div class="panel-body">
+				    <form method="post">
+						  <div class="form-group">
+						    <label for="kd">ID Alternatif</label>
+						    <select class="form-control" id="kd" name="kd">
+									<option>A1</option>
+									<option>A2</option>
+									<option>A3</option>
+									<option>A4</option>
+									<option>A5</option>
+								</select>
+						  </div>
+						  <div class="form-group">
+						    <label for="kt">Nama Alternatif</label>
+						    <input type="text" class="form-control" id="nm" name="nm" required>
+						  </div>
+							<div class="btn-group">
+							  <button type="submit" class="btn btn-dark">Simpan</button>
+							  <button type="button" onclick="location.href='data-alternatif.php'" class="btn btn-default">Kembali</button>
+							</div>
+					</form>
+			  </div>
 		</div>
-		<?php
-include_once 'footer.php';
-?>
+	</div>
+</div>
+
+<?php include_once('includes/footer.inc.php'); ?>
--- includes/alternatif.inc.php
+++ includes/alternatif.inc.php
@@ -1,39 +1,39 @@
 <?php
 class Alternatif{
-	
+
 	private $conn;
 	private $table_name = "ahp_data_alternatif";
-	
+
 	public $id;
 	public $nm;
 	public $sa;
 	public $hs;
-	
+
 	public function __construct($db){
 		$this->conn = $db;
 	}
-	
+
 	function insert(){
-		
+
 		$query = "insert into ".$this->table_name." values(?,?,'')";
 		$stmt = $this->conn->prepare($query);
 		$stmt->bindParam(1, $this->id);
 		$stmt->bindParam(2, $this->nm);
-		
+
 		if($stmt->execute()){
 			return true;
 		}else{
 			return false;
 		}
-		
+
 	}
-	
+
 	function readAll(){
 
 		$query = "SELECT * FROM ".$this->table_name." ORDER BY id_alternatif ASC";
 		$stmt = $this->conn->prepare( $query );
 		$stmt->execute();
-		
+
 		return $stmt;
 	}
 	function countAll(){
@@ -41,12 +41,12 @@ class Alternatif{
 		$query = "SELECT * FROM ".$this->table_name." ORDER BY id_alternatif ASC";
 		$stmt = $this->conn->prepare( $query );
 		$stmt->execute();
-		
+
 		return $stmt->rowCount();
 	}
-	
+
 	function readOne(){
-		
+
 		$query = "SELECT * FROM " . $this->table_name . " WHERE id_alternatif=? LIMIT 0,1";
 
 		$stmt = $this->conn->prepare( $query );
@@ -54,19 +54,19 @@ class Alternatif{
 		$stmt->execute();
 
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
-		
+
 		$this->id = $row['id_alternatif'];
 		$this->nm = $row['nama_alternatif'];
-		$this->sa = $row['skor_alternatif'];
+		// $this->sa = $row['skor_alternatif'];
 		$this->hs = $row['hasil_akhir'];
 	}
-	
+
 	// update the product
 	function update(){
 
-		$query = "UPDATE 
-					" . $this->table_name . " 
-				SET 
+		$query = "UPDATE
+					" . $this->table_name . "
+				SET
 					nama_alternatif = :nm
 				WHERE
 					id_alternatif = :id";
@@ -75,7 +75,7 @@ class Alternatif{
 
 		$stmt->bindParam(':nm', $this->nm);
 		$stmt->bindParam(':id', $this->id);
-		
+
 		// execute the query
 		if($stmt->execute()){
 			return true;
@@ -83,12 +83,12 @@ class Alternatif{
 			return false;
 		}
 	}
-	
+
 	// delete the product
 	function delete(){
-	
+
 		$query = "DELETE FROM " . $this->table_name . " WHERE id_alternatif = ?";
-		
+
 		$stmt = $this->conn->prepare($query);
 		$stmt->bindParam(1, $this->id);
 
@@ -99,9 +99,9 @@ class Alternatif{
 		}
 	}
 	function hapusell($ax){
-	
+
 		$query = "DELETE FROM " . $this->table_name . " WHERE id_alternatif in $ax";
-		
+
 		$stmt = $this->conn->prepare($query);
 
 		if($result = $stmt->execute()){
