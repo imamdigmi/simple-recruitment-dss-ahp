14aa7c9055cbb4685ee61252410508af3c34f986
imamdigmi <imam.digmi@gmail.com>
2017-07-06 22:11:44 +0700

    Update: Footer & Nilai

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,4 +1,5 @@
-<footer class="text-center">&copy; 2015</footer>
+	<hr>
+	<footer class="text-center">Copyright &copy; 2017 PT. Global Eushanosoft</footer>
 	</div>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="assets/js/jquery.dataTables.min.js"></script>
