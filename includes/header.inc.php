34c7ffcfdd8700da71b61488b86456339ba65bf9
imamdigmi <imam.digmi@gmail.com>
2017-07-15 20:15:39 +0700

    Close#6

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -59,9 +59,6 @@ $db = $config->getConnection();
                     <li role="presentation"><a href="data-kriteria.php">Kriteria</a></li>
                     <li role="presentation"><a href="nilai.php">Nilai</a></li>
                     <li role="presentation"><a href="nilai-awal.php">Nilai Awal</a></li>
-                <?php endif; ?>
-
-                <?php if ($_SESSION["role"] == "wakil" OR $_SESSION["role"] == "ketua"): ?>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Analisa <span class="caret"></span></a>
                         <ul class="dropdown-menu">
@@ -69,6 +66,8 @@ $db = $config->getConnection();
                             <li role="presentation"><a href="analisa-alternatif.php">Alternatif</a></li>
                         </ul>
                     </li>
+                <?php endif; ?>
+                <?php if ($_SESSION["role"] == "wakil" OR $_SESSION["role"] == "ketua"): ?>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Laporan <span class="caret"></span></a>
                         <ul class="dropdown-menu">
